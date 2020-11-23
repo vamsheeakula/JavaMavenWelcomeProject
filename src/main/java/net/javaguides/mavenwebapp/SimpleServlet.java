71323063404a49ad7f000012626d6258d4a025f8
Vamshee Akula <vakula@deloitte.co.uk>
2020-11-23 13:17:15 +0000

    Simple addition-9

--- src/main/java/net/javaguides/mavenwebapp/SimpleServlet.java
+++ src/main/java/net/javaguides/mavenwebapp/SimpleServlet.java
@@ -19,6 +19,6 @@ public class SimpleServlet extends HttpServlet {
    protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
          throws ServletException, IOException {
       resp.setContentType("text/plain");
-      resp.getWriter().write("Hello World!!!! Java Maven Web Project Example.");
+      resp.getWriter().write("Hello World!!!!> Java Maven Web Project Example.");
    }
 }
